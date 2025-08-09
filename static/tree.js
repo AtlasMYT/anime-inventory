60873afdcaee3d60f6f55f2917753269fb971394
MYT <99574999+IrisMYT@users.noreply.github.com>
2025-08-09 18:16:27 +0330

    Update tree.js

--- static/tree.js
+++ static/tree.js
@@ -1,12 +1,24 @@
 function renderTree(node, container) {
     const ul = document.createElement('ul');
+
     for (const key in node.children) {
+        const data = node.children[key];
         const li = document.createElement('li');
-        li.textContent = key;
-        if (Object.keys(node.children[key].children).length > 0) {
-            renderTree(node.children[key], li);
+
+        // Add icon for file/folder
+        if (data.type === "folder") {
+            li.textContent = "📁 " + key;
+        } else {
+            li.textContent = "📄 " + key;
         }
+
+        // Recurse for folders with children
+        if (data.type === "folder" && data.children && Object.keys(data.children).length > 0) {
+            renderTree(data, li);
+        }
+
         ul.appendChild(li);
     }
+
     container.appendChild(ul);
 }
